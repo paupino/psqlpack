1a17a11edcf91cd3b8118b066aafa2ac670c012b
Justin Latimer <justinlatimer@gmail.com>
2017-05-09 05:05:21 +0000

    Renames in psqlpack.sh

---
+++ psqlpack.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+case $2 in
+    simple)
+        db="simple"
+        ;;
+    complex)
+        db="complex"
+        ;;
+    *)
+        echo "Unsupported project: $2 (Try: simple, complex)"
+        exit 1
+        ;;
+esac
+
+username=`whoami`
+
+pushd ./cli > /dev/null
+case $1 in
+    package)
+        echo "Packaging '$db'"
+        cargo run -- package --source ../samples/$db/project.json --out ../out/$db.psqlpack
+        ;;
+    publish)
+        action="Publishing '$db'"
+        cargo run -- publish --source ../out/$db.psqlpack --target "host=localhost;database=$db;userid=$username;tlsmode=none;" --profile ../samples/$db/publish_profile.json
+        ;;
+    script)
+        action="Generating SQL for '$db'"
+        cargo run -- script --source ../out/$db.psqlpack --target "host=localhost;database=$db;userid=$username;tlsmode=none;" --profile ../samples/$db/publish_profile.json --out ../out/$db.sql
+        ;;
+    report)
+        action="Generating Report for '$db'"
+        cargo run -- report --source ../out/$db.psqlpack --target "host=localhost;database=$db;userid=$username;tlsmode=none;" --profile ../samples/$db/publish_profile.json --out ../out/$db.json
+        ;;
+    extract)
+        action="Extracting psqlpack for '$db'"
+        unzip ../out/$db.psqlpack -d ../out/$db
+        ;;
+    *)
+        echo "Unsupported command: $1 (Try: package, publish, script, report, extract)"
+        exit 1
+        ;;
+esac
+popd > /dev/null
