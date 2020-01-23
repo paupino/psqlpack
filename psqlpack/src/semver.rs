5dbf35c24ba3ca07af03d28b0ea5096081df59dd
Paul Mason <paul@form1.co.nz>
2020-01-22 20:47:53 -0800

    Removed duplicate PartialEq

--- psqlpack/src/semver.rs
+++ psqlpack/src/semver.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 
 use regex::Regex;
 
-#[derive(Clone, Copy, Debug, Hash, PartialEq)]
+#[derive(Clone, Copy, Debug, Hash)]
 pub struct Semver {
     major: u32,
     minor: u32,
