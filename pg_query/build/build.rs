e27fbd53b8996530f4fbfd2862b8b967215e6856
Paul Mason <paul@paulmason.me>
2021-05-01 16:13:01 +1200

    Playing around with protobuf generation

--- pg_query/build/build.rs
+++ pg_query/build/build.rs
@@ -1,19 +1,28 @@
-mod types;
-
-use std::collections::HashMap;
+use protobuf_codegen_pure::{Codegen, Customize};
 use std::env;
-use std::fs::File;
-use std::io::{prelude::*, BufReader, BufWriter, Write};
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
 
-use types::{Enum, Struct};
-
-const VERSION: &'static str = "10-1.0.2";
+const VERSION: &'static str = "13-2.0.4";
 
 fn main() {
-    let data = compile_pg_query();
-    generate_pg_query_types(&data);
+    let code_dir = compile_pg_query();
+
+    let out_dir = format!("{}/pg_query", env::var("OUT_DIR").unwrap());
+    if std::path::Path::new(&out_dir).exists() {
+        std::fs::remove_dir_all(&out_dir).unwrap();
+    }
+    std::fs::create_dir(&out_dir).unwrap();
+    Codegen::new()
+        .out_dir(out_dir)
+        .input(code_dir.join("protobuf/pg_query.proto"))
+        .include(code_dir.join("protobuf"))
+        .customize(Customize {
+            gen_mod_rs: Some(true),
+            ..Default::default()
+        })
+        .run()
+        .expect("protoc");
 }
 
 fn compile_pg_query() -> PathBuf {
@@ -45,182 +54,7 @@ fn compile_pg_query() -> PathBuf {
 
     println!("cargo:rustc-link-search=native={}", build_dir.display());
     println!("cargo:rustc-link-lib=static=pg_query");
-    build_dir.join("srcdata")
-}
-
-fn generate_pg_query_types(dir: &PathBuf) {
-    // Common out dir
-    let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
-    let out_file = File::create(out_dir.join("types.rs")).unwrap();
-    let mut out_file = BufWriter::new(out_file);
-    write_header(&mut out_file);
-
-    // First do enums
-    let enum_defs = File::open(dir.join("enum_defs.json")).unwrap();
-    let enum_defs = BufReader::new(enum_defs);
-    let enum_defs: HashMap<String, HashMap<String, Enum>> = serde_json::from_reader(enum_defs).unwrap();
-    make_enums(&enum_defs, &mut out_file);
-
-    // Next do structs
-    let struct_defs = File::open(dir.join("struct_defs.json")).unwrap();
-    let struct_defs = BufReader::new(struct_defs);
-    let struct_defs: HashMap<String, HashMap<String, Struct>> = serde_json::from_reader(struct_defs).unwrap();
-    make_nodes(&struct_defs, &mut out_file);
-
-    write_footer(&mut out_file);
-}
-
-fn write_header(out: &mut BufWriter<File>) {
-    write!(out, "pub use __pg_query::*;\n\n").unwrap();
-    write!(out, "mod __pg_query {{\n").unwrap();
-    write!(out, "    #![allow(non_snake_case, non_camel_case_types, unused_mut, unused_variables, unused_imports, unused_parens)]\n").unwrap();
-    write!(out, "\n").unwrap();
-
-    write!(out, "    use libc::{{c_char, c_int}};\n").unwrap();
-    write!(out, "    use uuid::Uuid;\n\n").unwrap();
-
-    write!(out, "    type bits32 = u32;\n").unwrap();
-    write!(out, "    type AclMode = u32;\n\n").unwrap();
-
-    // Read in the types to include
-    let out_dir = std::env::current_dir().unwrap();
-    let file = File::open(out_dir.join("types.inc")).unwrap();
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        write!(out, "    {}\n", line.unwrap()).unwrap();
-    }
-    write!(out, "\n").unwrap();
-}
-
-fn write_footer(out: &mut BufWriter<File>) {
-    write!(out, "}}\n").unwrap();
-}
-
-fn make_enums(enum_defs: &HashMap<String, HashMap<String, Enum>>, out: &mut BufWriter<File>) {
-    let sections = vec![
-        "nodes/parsenodes",
-        "nodes/primnodes",
-        "nodes/lockoptions",
-        "nodes/nodes",
-    ];
-    for section in sections {
-        for (name, def) in &enum_defs[section] {
-            write!(out, "    #[derive(Debug)]\n").unwrap();
-            write!(out, "    pub enum {} {{\n", name).unwrap();
-
-            for value in &def.values {
-                if let Some(comment) = &value.comment {
-                    write!(out, "        {}\n", comment).unwrap();
-                }
-                if let Some(name) = &value.name {
-                    write!(out, "        {},\n", name).unwrap();
-                }
-            }
-            write!(out, "    }}\n\n").unwrap();
-        }
-    }
-}
-
-fn make_nodes(struct_defs: &HashMap<String, HashMap<String, Struct>>, out: &mut BufWriter<File>) {
-    write!(out, "    #[derive(Debug)]\n").unwrap();
-    write!(out, "    pub enum Node {{\n").unwrap();
-    for (name, def) in &struct_defs["nodes/parsenodes"] {
-        write!(out, "        {} {{\n", name).unwrap();
-
-        for field in &def.fields {
-            let (name, c_type) = match (&field.name, &field.c_type) {
-                (&Some(ref name), &Some(ref c_type)) => (name, c_type),
-                _ => continue,
-            };
-
-            if name == "type" {
-                continue;
-            }
-            write!(
-                out,
-                "            {}: {},\n",
-                remove_reserved_keyword(name),
-                c_to_rust_type(c_type)
-            )
-            .unwrap();
-        }
-
-        write!(out, "        }},\n").unwrap();
-    }
-
-    write!(out, "    }}\n").unwrap();
-}
-
-fn remove_reserved_keyword(variable: &str) -> String {
-    if is_reserved(variable) {
-        format!("{}_", variable)
-    } else {
-        variable.into()
-    }
-}
-
-fn is_reserved(variable: &str) -> bool {
-    match variable {
-        "abstract" | "become" | "box" | "do" | "final" | "macro" | "override" | "priv" | "try" | "typeof"
-        | "unsized" | "virtual" | "yield" => true,
-        _ => false,
-    }
-}
-
-fn c_to_rust_type(c_type: &str) -> &str {
-    match c_type {
-        // Primitive mappings
-        "uint32" => "u32",
-        "bool" => "bool",
-        "int" => "i32",
-        "long" => "i64",
-        "int32" => "i32",
-        "char*" => "String",
-        "int16" => "i16",
-        "char" => "u8",
-        "double" => "f64",
-
-        // Vec
-        "List*" => "Vec<Node>",
-
-        // Box
-        "Node*" => "Box<Node>",
-        "Alias*" => "Box<Node>",
-        "Bitmapset*" => "Box<Node>",
-        "CollateClause*" => "Box<Node>",
-        "CreateStmt" => "Box<Node>",
-        "Expr*" => "Box<Node>",
-        "FromExpr*" => "Box<Node>",
-        "GrantStmt*" => "Box<Node>",
-        "Index" => "Box<Node>",
-        "InferClause*" => "Box<Node>",
-        "IntoClause*" => "Box<Node>",
-        "ObjectWithArgs*" => "Box<Node>",
-        "Oid" => "Uuid",
-        "OnConflictClause*" => "Box<Node>",
-        "OnConflictExpr*" => "Box<Node>",
-        "PartitionSpec*" => "Box<Node>",
-        "PartitionBoundSpec*" => "Box<Node>",
-        "Query*" => "Box<Node>",
-        "RangeVar*" => "Box<Node>",
-        "RoleSpec*" => "Box<Node>",
-        "SelectStmt*" => "Box<Node>",
-        "TableFunc*" => "Box<Node>",
-        "TableSampleClause*" => "Box<Node>",
-        "TypeName*" => "Box<Node>",
-        "VariableSetStmt*" => "Box<Node>",
-        "WindowDef*" => "Box<Node>",
-        "WithClause*" => "Box<Node>",
-
-        // Generic
-        other => {
-            if other.ends_with("*") {
-                other[0..other.len() - 1].into()
-            } else {
-                other
-            }
-        }
-    }
+    build_dir
 }
 
 fn run_command(command: &mut Command) {
--- pg_query/src/lib.rs
+++ pg_query/src/lib.rs
@@ -1,112 +1,23 @@
-extern crate libc;
+mod pg_query {
+    include!(concat!(env!("OUT_DIR"), "/pg_query/mod.rs"));
 
-pub mod pg_types {
-    include!(concat!(env!("OUT_DIR"), "/types.rs"));
+    use pg_query::*;
 }
 
-use libc::{c_char, c_int};
-
-use std::ffi::{CStr, CString};
-use std::fmt;
-
-use pg_types::Node;
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-struct PgQueryError {
-    pub message: *mut c_char,
-    pub filename: *mut c_char,
-    pub lineno: c_int,
-    pub cursorpos: c_int,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-struct PgQueryParseResult {
-    pub parse_tree: *mut c_char,
-    pub stderr_buffer: *mut c_char,
-    pub error: *mut PgQueryError,
-}
-
-#[derive(Clone)]
-pub struct ParseError {
-    pub message: String,
-    pub file: String,
-    pub line: u32,
-    pub index: usize,
-    _p: (),
-}
-
-impl fmt::Debug for ParseError {
-    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
-        fmt.debug_struct("Error")
-            .field("message", &self.message)
-            .field("file", &self.file)
-            .field("line", &self.line)
-            .field("index", &self.index)
-            .finish()
-    }
-}
-
-impl ParseError {
-    unsafe fn from_raw(raw: *mut PgQueryError) -> ParseError {
-        ParseError {
-            message: std::str::from_utf8(CStr::from_ptr((*raw).message).to_bytes())
-                .unwrap()
-                .to_owned(),
-            file: std::str::from_utf8(CStr::from_ptr((*raw).filename).to_bytes())
-                .unwrap()
-                .to_owned(),
-            line: (*raw).lineno as u32,
-            index: (*raw).cursorpos as usize,
-            _p: (),
-        }
-    }
-}
-
-extern "C" {
-    fn pg_query_parse(input: *const c_char) -> PgQueryParseResult;
-    fn pg_query_free_parse_result(result: PgQueryParseResult);
-}
-
-fn parse_internal(query: &str) -> Result<String, ParseError> {
-    let query = CString::new(query).expect("interior null");
-    unsafe {
-        let raw_result = pg_query_parse(query.as_ptr() as *mut _);
-        let result = if raw_result.error.is_null() {
-            Ok(std::str::from_utf8(CStr::from_ptr(raw_result.parse_tree).to_bytes())
-                .unwrap()
-                .to_owned())
-        } else {
-            Err(ParseError::from_raw(raw_result.error))
-        };
-        pg_query_free_parse_result(raw_result);
-        result
-    }
-}
-
-pub fn parse(query: &str) -> Result<Node, ParseError> {
-    let json = parse_internal(query)?;
-    let root_node = serde_json::from_str(&json).unwrap_or_else(|_| {
-        return Err(ParseError {
-            message: "Failed to deserialize root node".into(),
-            file: String::new(),
-            line: 0,
-            index: 0,
-            _p: (),
-        });
-    })?;
-    root_node
-}
+pub fn parse(stmt: &str) {}
 
 #[cfg(test)]
 mod tests {
+    use crate::pg_query::ParseResult;
+
     #[test]
     fn it_works() {
-        println!(
-            "{:?}",
-            super::parse("CREATE INDEX ix_test ON contacts.person (id, ssn) WHERE ssn IS NOT NULL;").unwrap()
-        );
+        let mut result = ParseResult::new();
+        result.set_stmts("CREATE INDEX ix_test ON contacts.person (id, ssn) WHERE ssn IS NOT NULL;");
+        // println!(
+        //     "{:?}",
+        //     pg_query::parse("CREATE INDEX ix_test ON contacts.person (id, ssn) WHERE ssn IS NOT NULL;").unwrap()
+        // );
         assert!(false);
     }
 }
